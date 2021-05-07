c65fc4e0ea303baad71bebd3ab529a06204fae76
blue_bla <73012354+Blueblas@users.noreply.github.com>
2021-05-07 11:49:16 +0900

    Add files via upload

---
+++ turtle로 그림 그리기.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.shape("classic")
+turtle.forward(200)
+#forwoard = 앞으로 200만큼 이동
+turtle.right(90)
+#오른쪽으로 90 방향회전
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.done()
+
+#turtle = 거북이, circle = 원, classic = 화살표
