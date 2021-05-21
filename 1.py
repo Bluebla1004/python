1b9bf237e1777cf8ff15f797ae1e1d8dcafe78c2
blue_bla <73012354+Blueblas@users.noreply.github.com>
2021-05-21 10:54:38 +0900

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+while True:
+    a = int(input("정수1:"))
+    b = int(input("정수2:"))
+
+    if a>0:
+        if b>0:
+            print("positive number")
+        else:
+            print("negative number")
+    else:
+        if b>0:
+            print("negativ number")
+        else:
+            print("prositive number")
