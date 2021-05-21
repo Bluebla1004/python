49e55f6564d3fd911d9289dafddaf824a669fa4d
blue_bla <73012354+Blueblas@users.noreply.github.com>
2021-05-21 11:39:23 +0900

    Add files via upload

---
+++ BMI수치... .비만도 측정 프로그램 작성.py
@@ -0,0 +1,19 @@
+#BMI = 몸무게 / (키*키)
+#BMI지수 20 미만이면 저체중
+#BMI지수 20 이상24이하면 정상
+#BMI지수 25 이상30이하면 경도비만
+#BMI지수 31 이상이면 비만
+#입력 예시) 176cm 86kg
+
+kg = float(input("몸무게:"))
+cm = float(input("키:"))
+bmi = kg/(cm*cm)
+
+if bmi<20:
+    print ("저체중")
+elif 20<=bmi<=24:
+    print("정상체중")
+elif 25<=bmi<=30:
+    print("경도비만")
+else:
+    print("비만")
